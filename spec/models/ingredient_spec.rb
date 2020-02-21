7e3c78933ccac58d24442ccea19dd163ac079c3b
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-21 09:13:21 -0700

    Add model tests for many-to-many

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Ingredients, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :calories }
+  end
+
+  describe 'relationships' do
+    it { should have_many :dishes }
+    it { should have_many(:dishes).through(:dish_ingredients)}
+  end
+end
